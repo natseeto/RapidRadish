e9f02b253ad0b320823dfb5477d2c24f1cdb50e3
natseeto <61493185+natseeto@users.noreply.github.com>
2021-07-03 20:49:31 +1000

    Convert javafx
    add dependencies?? idk what i actually did tbh

---
+++ src/rapidRadish/HomeControllerRR.java
@@ -0,0 +1,33 @@
+package rapidRadish;
+
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.fxml.*;
+import javafx.event.*;
+import javafx.*;
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.*;
+
+
+public class HomeControllerRR implements Initializable {
+		@FXML
+		private Button button;
+		
+		
+		public void initialize(URL url, ResourceBundle rb) {
+			
+		}
+		
+		 @FXML
+		    private void about(ActionEvent event) throws IOException {
+		        Parent root = FXMLLoader.load(getClass().getResource("aboutV2.fxml"));
+		        Scene scene = new Scene(root);
+
+		        Stage stage = (Stage) bt_about.getScene().getWindow();
+		        stage.setScene(scene);
+		        stage.show();
+
+		    }
+}
